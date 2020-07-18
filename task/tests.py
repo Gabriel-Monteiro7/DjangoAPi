80c0b2aac389a1b3b7c2681230d57cb8d511fa00
GabrielMonteiro <gabrielmont713gmail.com>
2020-07-17 21:54:08 -0300

    Criaçao de um teste simples para as requisiçoes vinda da task

--- task/tests.py
+++ task/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+class TestTasks:
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk1MTAxOTE2LCJqdGkiOiIxZmZhNDhmYjg2Nzc0NjQwOTc0ZTY0MWI4MzkyYWE5OSIsInVzZXJfaWQiOjJ9.me_aZ4dKVuBAKZfjDuWBdfkccWHOt-987_vkpTVhs24'}
+    url_base_tasks = 'http://localhost:8000/api/tasks/'
+
+    def test_get_tasks(self):
+        resposta = requests.get(url=self.url_base_tasks, headers=self.headers)
+
+        assert resposta.status_code == 200
+
+    def test_get_task(self):
+        resposta = requests.get(
+            url=f'{self.url_base_tasks}21/', headers=self.headers)
+
+        assert resposta.status_code == 200
+
+    def test_post_task(self):
+        task = {
+            'name': 'Teste1',
+            'deadline': '2020-07-17T21:37:50.907312-03:00',
+            'priority': 'Alta',
+            'description': 'test',
+        }
+        resposta = requests.post(
+            url=self.url_base_tasks, headers=self.headers, json=task)
+        assert resposta.status_code == 201
+        assert resposta.json()['name'] == task['name']
+
+    def test_put_task(self):
+        task = {
+            'name': 'Teste 2',
+            'deadline': '2020-07-17T21:37:50.907312-03:00',
+            'priority': 'Alta',
+            'description': 'test',
+        }
+
+        resposta = requests.put(
+            url=f'{self.url_base_tasks}21/', headers=self.headers, json=task)
+
+        assert resposta.status_code == 200
+
+    def test_delete_task(self):
+        resposta = requests.delete(
+            url=f'{self.url_base_tasks}25/', headers=self.headers)
+
+        assert resposta.status_code == 204 and len(resposta.text) == 0
--- task/views.py
+++ task/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-
-from rest_framework import viewsets, permissions
-
+from rest_framework import viewsets, permissions, status, mixins
+from django.shortcuts import get_object_or_404
 from .models import Task
 from .serializers import TaskSerializer
-
 from rest_framework.response import Response
 
 
@@ -14,9 +12,43 @@ class TaskViewSet(viewsets.ModelViewSet):
         permissions.IsAuthenticated,
     ]
 
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
     def list(self, request):
         queryset = Task.objects.all().filter(user=request.user.id)
         serializer = TaskSerializer(queryset, many=True)
         return Response(serializer.data)
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+
+    def create(self, request):
+        print(request.data)
+        request.data['user'] = request.user.id
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        queryset = Task.objects.all().filter(user=request.user.id)
+        task = get_object_or_404(queryset, pk=pk)
+        request.data['user'] = request.user.id
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Task.objects.all().filter(user=request.user.id)
+        task = get_object_or_404(queryset, pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk, format=None):
+        queryset = Task.objects.all().filter(user=request.user.id)
+        task = get_object_or_404(queryset, pk=pk)
+        if(task.user.id == request.user.id):
+            task.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_404_NOT_FOUND)
